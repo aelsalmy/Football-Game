35e680afbaeda0c1ade8aef14914bea0d152ca82
aelsalmy <abdeullaelsalmy2003@gmail.com>
2023-12-18 16:15:24 +0200

    Added Dropping Objects : Karim

--- src/finalproject/Model/Game/Game.java
+++ src/finalproject/Model/Game/Game.java
@@ -7,6 +7,7 @@ import finalproject.Model.Objects.*;
 import finalproject.Model.Players.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Game implements World , AvoidableHitObservor{
     
@@ -22,10 +23,12 @@ public class Game implements World , AvoidableHitObservor{
     private final List<GameObject> controlled = new LinkedList();
     private int speed = 10;
     private int controlSpeed = 10;
+    private Random random = new Random();
 
     
     public Game(Player p){
         this.stateOfGame = new GameRunning();
+        this.factory = ObjectFactory.getInstance();
         controlled.add(p);
         constants.add(new Whistle(10 , 22));
         constants.add(new YellowCard(50 , 2));
@@ -60,9 +63,25 @@ public class Game implements World , AvoidableHitObservor{
 
     @Override
     public boolean refresh() {
+    
+        if (random.nextInt(100) < 1.2) {
+            GameObject collectableObject = factory.generateRandomCollectable(0, 0);
+            collectableObject.setX(random.nextInt(getWidth()));
+            collectableObject.setY(0);
+            movables.add(collectableObject);
+        }
+        if (random.nextInt(100) < 0.5) {
+            GameObject avoidableObject = factory.generateRandomAvoidable(0, 0);
+            avoidableObject.setX(random.nextInt(getWidth()));
+            avoidableObject.setY(0);
+            movables.add(avoidableObject);
+        }
+        for (GameObject obj : movables.toArray(GameObject[]::new)) {
+            obj.setY(obj.getY() + speed);
+        }
         return stateOfGame.refreshGame();
     }
-
+    
     @Override
     public String getStatus() {
         return " Game is Running 7ad yel7a2o hhhhh";
--- src/finalproject/Model/Objects/ObjectFactory.java
+++ src/finalproject/Model/Objects/ObjectFactory.java
@@ -5,20 +5,50 @@
 package finalproject.Model.Objects;
 
 import eg.edu.alexu.csd.oop.game.GameObject;
+import java.util.Random;
+
 
 /**
  *
  * @author abdul
  */
 public class ObjectFactory {
-    
-    public GameObject generateRandomAvoidable(){
-        //TODO: Logic to return random Avoidable
-        return null;
+    private static ObjectFactory instance;
+    private final Random random;
+
+    private ObjectFactory() {
+        random = new Random();
+    }
+
+    public static ObjectFactory getInstance() {
+        if (instance == null) {
+            instance = new ObjectFactory();
+        }
+        return instance;
     }
     
-    public GameObject generateRandomCollectable(){
-        //TODO: Logic to return random Collectable
-        return null;
+    public GameObject generateRandomAvoidable(int x, int y) {
+        // Use random logic to choose the type of avoidable object
+        int randomType = random.nextInt(2);
+
+        // Create and return the corresponding object
+        return switch (randomType) {
+            case 0 -> new Injury(x, y);
+            case 1 -> new WaterBottle(x, y);
+            default -> null;
+        };
+    }
+
+    public GameObject generateRandomCollectable(int x, int y) {
+        // Use random logic to choose the type of collectable object
+        int randomType = random.nextInt(3); 
+
+        // Create and return the corresponding object
+        return switch (randomType) {
+            case 0 -> new BallonDor(x, y);
+            case 1 -> new Ball(x, y,BallTypes.Jabulani);
+            case 2 -> new Ball(x,y, BallTypes.Rihla);
+            default -> null;
+        };
     }
 }
